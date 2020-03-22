7c1f360121526d0eb240003998aee5b282384e8a
Oxana Kochueva <oxanak99@gmail.com>
2020-03-22 13:31:14 +0300

    dip confetti styles

--- app/javascript/containers/Chat.jsx
+++ app/javascript/containers/Chat.jsx
@@ -109,6 +109,53 @@ export default class Chat extends React.Component {
             </div>
           </div>
         </div>
+        <div className="dipConfetti hidden">
+          <div className="y">
+            <div className="dipSmall">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipMedium">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipLarge">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipSmall dipSmallSecond">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipMedium dipMediumSecond">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipLarge dipLargeSecond">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipSmall dipSmallThird">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipMedium dipMediumThird">
+              <div className="x"></div>
+            </div>
+          </div>
+          <div className="y">
+            <div className="dipLarge dipLargeThird">
+              <div className="x"></div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
